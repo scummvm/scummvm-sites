78dc00218429210bbb443c65ce97baf92d79b2f4
D G Turner <digitall@scummvm.org>
2012-11-22 20:45:45 +0000

    BUILDBOT: Add tools nightly links to builds download page.

--- public_html/update_builds_html.pl
+++ public_html/update_builds_html.pl
@@ -24,6 +24,7 @@ sub add_platform {
 	my $icon = shift;
 	my $file_abbrev = shift;
 	my $desc = shift;
+	my $build_tools = shift;
 	# TODO: Display more info, e.g. file sizes and build date (that would require using
 	# PHP or SSI or something like that)
 
@@ -47,35 +48,46 @@ sub add_platform {
 		print FILE '</td>';
 	}
 
+	# tools master daily builds
+	if (1) {
+		print FILE '<td style="text-align: center; width: 20em;" class="row1">';
+		if ($build_tools) {
+			print FILE '<a href="/snapshots/tools-master/' . $file_abbrev . '-tools-master-latest.tar.bz2">Download latest tools build</a>';
+		} else {
+			print FILE 'N/A';
+		}
+		print FILE '</td>';
+	}
+
 	print FILE '</tr>';
 }
 
-add_platform("amiga", "amigaos4", "Amiga OS4");
-add_platform("android", "android", "Android");
-add_platform("dc", "dc", "Dreamcast plain files");
-add_platform("debian", "debian-x86", "Debian 32bit");
-add_platform("debian", "debian-x86_64", "Debian 64bit");
-add_platform("dingux", "dingux", "Dingux");
-add_platform("n64", "n64", "Nintendo 64");
-add_platform("ds", "ds", "Nintendo DS");
-add_platform("gc", "gamecube", "Nintendo Gamecube");
-add_platform("wii", "wii", "Nintendo Wii");
-add_platform("caanoo", "caanoo", "GamePark Caanoo");
-add_platform("gp2x", "gp2x", "GamePark GP2X");
-add_platform("gp2xwiz", "gp2xwiz", "GamePark GP2XWiz");
-add_platform("iphone", "iphone", "iPhone");
-add_platform("macos-universal", "osx_intel", "Mac OS X (Intel)");
-add_platform("macos-universal", "osx_ppc", "Mac OS X (PowerPC)");
-add_platform("linuxmoto", "motoezx", "Motorola (MotoEZX)");
-add_platform("linuxmoto", "motomagx", "Motorola (MotoMAGX)");
-add_platform("openpandora", "openpandora", "OpenPandora");
-add_platform("ps2", "ps2", "Playstation 2");
-add_platform("ps3", "ps3", "Playstation 3");
-add_platform("psp", "psp", "Playstation Portable");
-add_platform("webos", "webos", "HP webOS");
-add_platform("windows", "mingw-w32", "Windows (32bit)");
-add_platform("win64", "mingw-w64", "Windows (64bit)");
-add_platform("wince", "wince", "Windows CE (ARM)");
+add_platform("amiga", "amigaos4", "Amiga OS4", "0");
+add_platform("android", "android", "Android", "0");
+add_platform("dc", "dc", "Dreamcast plain files", "0");
+add_platform("debian", "debian-x86", "Debian 32bit", "1");
+add_platform("debian", "debian-x86_64", "Debian 64bit", "1");
+add_platform("dingux", "dingux", "Dingux", "0");
+add_platform("n64", "n64", "Nintendo 64", "0");
+add_platform("ds", "ds", "Nintendo DS", "0");
+add_platform("gc", "gamecube", "Nintendo Gamecube", "0");
+add_platform("wii", "wii", "Nintendo Wii", "0");
+add_platform("caanoo", "caanoo", "GamePark Caanoo", "0");
+add_platform("gp2x", "gp2x", "GamePark GP2X", "0");
+add_platform("gp2xwiz", "gp2xwiz", "GamePark GP2XWiz", "0");
+add_platform("iphone", "iphone", "iPhone", "0");
+add_platform("macos-universal", "osx_intel", "Mac OS X (Intel)", "1");
+add_platform("macos-universal", "osx_ppc", "Mac OS X (PowerPC)", "1");
+add_platform("linuxmoto", "motoezx", "Motorola (MotoEZX)", "0");
+add_platform("linuxmoto", "motomagx", "Motorola (MotoMAGX)", "0");
+add_platform("openpandora", "openpandora", "OpenPandora", "0");
+add_platform("ps2", "ps2", "Playstation 2", "0");
+add_platform("ps3", "ps3", "Playstation 3", "0");
+add_platform("psp", "psp", "Playstation Portable", "0");
+add_platform("webos", "webos", "HP webOS", "0");
+add_platform("windows", "mingw-w32", "Windows (32bit)", "1");
+add_platform("win64", "mingw-w64", "Windows (64bit)", "1");
+add_platform("wince", "wince", "Windows CE (ARM)", "0");
 
 
 print FILE $html_footer;
