1dcb22bfb91c775146025fa67c31a023d12ef1bf
D G Turner <digitall@scummvm.org>
2014-03-01 18:57:10 +0000

    BUILDBOT: Add script to rebuild Android toolchain debug signing key.
    This needs to run just prior to the previous key expiration via a
    cron script on the server. The keys are set for expiration in 365
    days from generation i.e. 1 year.

---
+++ rebuild-android-debugkey.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd ~buildbot/.android
+mv -v debug.keystore debug.keystore-`date +%Y-%m-%d`
+#Make Android Debug Key Valid for 1 year
+keytool -genkey -v -keystore ./debug.keystore \
+-storepass android -alias androiddebugkey -keypass android \
+-dname "CN=Android Debug,O=Android,C=US" \
+-validity 365
+chown buildbot:nogroup debug.keystore
+
