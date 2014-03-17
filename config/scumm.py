8451d6e4e025b45afbf214a368c692d8942ff6c0
D G Turner <digitall@scummvm.org>
2014-03-17 01:22:55 +0000

    BUILDBOT: Prevent "Clean" buildstep being killed by timeout.
    The Clean buildstep has started failing as the "rm" command runs
    with no output, and due to the size of the builders directories. after
    1200 secs of no output, the buildbot framework assumes that the command
    has got stuck and kills the process and fails.
    
    This is preventing nightlies from running correctly.
    
    To prevent this, the verbose flag is added to the rm command to ensure
    output and prevent the timeout.

--- config/scumm.py
+++ config/scumm.py
@@ -204,7 +204,7 @@ class Clean(ShellCommand):
 		self.addFactoryArguments(prefix = self.prefix)
 
 	def start(self):
-		self.command = "rm -rf ../../%s-*" % self.prefix
+		self.command = "rm -vrf ../../%s-*" % self.prefix
 		ShellCommand.start(self)
 
 # IRC stuff
