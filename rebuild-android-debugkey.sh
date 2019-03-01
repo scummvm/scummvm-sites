44ccbd882e7cd322a06ceb37a865e04917d744fc
D G Turner <digitall@scummvm.org>
2019-03-01 18:27:03 +0000

    BUILDBOT: Workaround for Android Build Failure
    The debug signing key is too long for a SHA-1 signature so this fails.
    This is a workaround by reducing the key length. This should be fixed
    long term to use a more secure algorithm for signing with a long key
    length.

--- rebuild-android-debugkey.sh
+++ rebuild-android-debugkey.sh
@@ -5,6 +5,6 @@ mv -v debug.keystore debug.keystore-`date +%Y-%m-%d`
 keytool -genkey -v -keystore ./debug.keystore \
 -storepass android -alias androiddebugkey -keypass android \
 -dname "CN=Android Debug,O=Android,C=US" \
--validity 365
+-validity 365 \
+-keysize 1024
 chown buildbot:nogroup debug.keystore
-
