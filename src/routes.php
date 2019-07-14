2389a45bf8883652fe338203ae5d3b0eb03700d7
Matan Bareket <mataniko@gmail.com>
2019-07-14 12:17:46 -0400

    CLOUD: Initial commit with Dropbox support

---
+++ src/routes.php
@@ -0,0 +1,71 @@
+<?php
+
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+return function (App $app) {
+    $container = $app->getContainer();
+
+    $app->get(
+        '/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
+            // Sample log message
+            $container->get('logger')->info("Slim-Skeleton '/' route");
+
+            // Render index view
+            return $container->get('renderer')->render($response, 'index.phtml', $args);
+        }
+    );
+
+    $app->group(
+        '/dropbox',  function () use ($app, $container) {
+            $dropbox = $container->get('settings')['dropbox'];
+            $app->get(
+                '/auth', function (Request $request, Response $response, array $args) use ($app, $dropbox) {
+                    $authUri = "{$dropbox['auth_uri']}?response_type=code&redirect_uri={$dropbox['redirect_uri']}&client_id={$dropbox['client_id']}";
+                    return $response->withRedirect($authUri);
+                }
+            );
+
+            $app->get(
+                '/token', function (Request $request, Response $response, array $args) use ($app, $dropbox) {
+                    $code = $request->getQueryParam('code');
+                    $client = new \GuzzleHttp\Client();
+                    $res = $client->request(
+                        'POST',
+                        $dropbox['token_uri'],
+                        [
+                        'auth' => [
+                          $dropbox['client_id'],
+                          $dropbox['client_secret']
+                        ],
+                        'form_params' => [
+                          'grant_type' => 'authorization_code',
+                          'redirect_uri' => $dropbox['redirect_uri'],
+                          'code' => $code
+                        ]
+                        ]
+                    );
+
+                    $json = json_decode($res->getBody(), true);
+
+                    $this->random = new PragmaRX\Random\Random();
+                    $shortcode = $this->random->size(6)->get();
+                    $client = new Predis\Client();
+                    $client->set("cloud-{$shortcode}", $json['access_token']);
+                    $client->expire($shortcode, 600);
+                    return $response->withJson(['shortcode' => $shortcode, 'token' => $json['access_token']]);
+                }
+            );
+
+            $app->get(
+                '/token/{shortcode}', function (Request $request, Response $response, $args) {
+                    $client = new Predis\Client();
+                    $shortcode = $args['shortcode'];
+                    $token = $client->get("cloud-{$shortcode}");
+                    return $response->withJson(['shortcode' => $shortcode, 'token' => $token]);
+                }
+            );
+        }
+    );
+};
---
+++ src/settings.php
@@ -0,0 +1,59 @@
+<?php
+return [
+    'settings' => [
+        'displayErrorDetails' => true, // set to false in production
+        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+
+        // Renderer settings
+        'renderer' => [
+            'template_path' => __DIR__ . '/../templates/',
+        ],
+
+        // Monolog settings
+        'logger' => [
+            'name' => 'slim-app',
+            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+            'level' => \Monolog\Logger::DEBUG,
+        ],
+
+        // Dropbox settings
+        'dropbox' => [
+          'client_id' => 'vgij09edeilbrtm',
+          'client_secret' => isset($_ENV['DROPBOX_SECRET']) ? $_ENV['DROPBOX_SECRET'] : '',
+          'redirect_uri' => 'https://cloud.scummvm.org/dropbox/token',
+          'auth_uri' => 'https://www.dropbox.com/1/oauth2/authorize',
+          'token_uri' => 'https://api.dropboxapi.com/oauth2/token',
+          'refresh_uri' => ''
+        ],
+
+        // Box settings
+        'box' => [
+          'client_id' => 'ep9cz17to1wakzqbq2a5jn5u01b0omxw',
+          'client_secret' => isset($_ENV['BOX_SECRET']) ? $_ENV['BOX_SECRET'] : '',
+          'redirect_uri' => 'https://cloud.scummvm.org/box/token',
+          'auth_uri' => 'https://www.dropbox.com/1/oauth2/authorize',
+          'token_uri' => 'https://api.dropboxapi.com/oauth2/token',
+          'refresh_uri' => ''
+        ],
+
+        // Google Drive settings
+        'google_drive' => [
+          'client_id' => '102752616291108817673',
+          'client_secret' => isset($_ENV['GOOGLE_DRIVE_SECRET']) ? $_ENV['GOOGLE_DRIVE_SECRET'] : '',
+          'redirect_uri' => 'https://cloud.scummvm.org/google_drive/token',
+          'auth_uri' => 'https://accounts.google.com/o/oauth2/auth',
+          'token_uri' => 'https://oauth2.googleapis.com/token',
+          'refresh_uri' => ''
+        ],
+
+        // Onedrive settings
+        'onedrive' => [
+          'client_id' => '12c88b6d-3037-4c0c-9076-cc4205cfb1d0',
+          'client_secret' => isset($_ENV['ONEDRIVE_SECRET']) ? $_ENV['ONEDRIVE_SECRET'] : '',
+          'redirect_uri' => 'https://cloud.scummvm.org/onedrive/token',
+          'auth_uri' => 'https://www.dropbox.com/1/oauth2/authorize',
+          'token_uri' => 'https://api.dropboxapi.com/oauth2/token',
+          'refresh_uri' => ''
+        ],
+    ],
+];
