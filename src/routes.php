94b494c55f254d05f8ad38e7ef3085a36255db5a
Matan Bareket <mataniko@gmail.com>
2019-07-14 21:34:34 -0400

    CLOUD: Use existing oauth clients

--- src/routes.php
+++ src/routes.php
@@ -8,7 +8,7 @@ return function (App $app) {
     $container = $app->getContainer();
 
     $app->get(
-        '/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
+        '/', function (Request $request, Response $response, array $args) use ($container) {
             // Sample log message
             $container->get('logger')->info("Slim-Skeleton '/' route");
 
@@ -17,55 +17,97 @@ return function (App $app) {
         }
     );
 
-    $app->group(
-        '/dropbox',  function () use ($app, $container) {
-            $dropbox = $container->get('settings')['dropbox'];
-            $app->get(
-                '/auth', function (Request $request, Response $response, array $args) use ($app, $dropbox) {
-                    $authUri = "{$dropbox['auth_uri']}?response_type=code&redirect_uri={$dropbox['redirect_uri']}&client_id={$dropbox['client_id']}";
-                    return $response->withRedirect($authUri);
-                }
-            );
+    $app->get(
+        '/{cloud}', function (Request $request, Response $response, array $args) use ($container) {
+            $cloud = $args['cloud'];
+            $oauth = $container->get('settings')[$cloud];
+            switch ($cloud) {
+            case 'dropbox':
+                $provider = new Stevenmaguire\OAuth2\Client\Provider\Dropbox(
+                    [
+                    'clientId'          => $oauth['client_id'],
+                    'clientSecret'      => $oauth['client_secret'],
+                    'redirectUri'       => $oauth['redirect_uri']
+                    ]
+                );
+                break;
+            case 'box':
+                $provider = new Stevenmaguire\OAuth2\Client\Provider\Box(
+                    [
+                    'clientId'          => $oauth['client_id'],
+                    'clientSecret'      => $oauth['client_secret'],
+                    'redirectUri'       => $oauth['redirect_uri']
+                    ]
+                );
+                break;
+            case 'gdrive':
+                $provider = new League\OAuth2\Client\Provider\Google(
+                    [
+                    'clientId'          => $oauth['client_id'],
+                    'clientSecret'      => $oauth['client_secret'],
+                    'redirectUri'       => $oauth['redirect_uri']
+                    ]
+                );
+                break;
+            case 'onedrive':
+                $provider = new Stevenmaguire\OAuth2\Client\Provider\Microsoft(
+                    [
+                    'clientId'          => $oauth['client_id'],
+                    'clientSecret'      => $oauth['client_secret'],
+                    'redirectUri'       => $oauth['redirect_uri']
+                    ]
+                );
+                break;
+            default:
+                return;
+                break;
+            }
 
-            $app->get(
-                '/token', function (Request $request, Response $response, array $args) use ($app, $dropbox, $container) {
-                    $code = $request->getQueryParam('code');
-                    $client = new \GuzzleHttp\Client();
-                    $res = $client->request(
-                        'POST',
-                        $dropbox['token_uri'],
-                        [
-                        'auth' => [
-                          $dropbox['client_id'],
-                          $dropbox['client_secret']
-                        ],
-                        'form_params' => [
-                          'grant_type' => 'authorization_code',
-                          'redirect_uri' => $dropbox['redirect_uri'],
-                          'code' => $code
-                        ]
-                        ]
-                    );
+            if (!isset($_GET['code'])) {
+                // If we don't have an authorization code then get one
+                $authUrl = $provider->getAuthorizationUrl();
+                $_SESSION['oauth2state'] = $provider->getState();
 
-                    $json = json_decode($res->getBody(), true);
+                return $response->withRedirect($authUrl);
 
-                    $this->random = new PragmaRX\Random\Random();
-                    $shortcode = $this->random->size(6)->get();
-                    $client = new Predis\Client();
-                    $client->set("cloud-dropbox-{$shortcode}", $json['access_token']);
-                    $client->expire("cloud-dropbox-{$shortcode}", 600);
-                    return $container->get('renderer')->render($response, 'token.phtml', ['shortcode' => $shortcode]);
-                }
-            );
+                // Check given state against previously stored one to mitigate CSRF attack
+            } elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
 
-            $app->get(
-                '/token/{shortcode}', function (Request $request, Response $response, $args) {
-                    $client = new Predis\Client();
-                    $shortcode = $args['shortcode'];
-                    $token = $client->get("cloud-dropbox-{$shortcode}");
-                    return $response->withJson(['error' => false, 'access_token' => $token]);
-                }
-            );
+                unset($_SESSION['oauth2state']);
+                exit('Invalid state');
+
+            } else {
+
+                // Try to get an access token (using the authorization code grant)
+                $token = $provider->getAccessToken(
+                    'authorization_code', [
+                    'code' => $_GET['code']
+                    ]
+                );
+
+
+                // Use this to interact with an API on the users behalf
+                // echo $token->getToken();
+
+                $this->random = new PragmaRX\Random\Random();
+                $shortcode = $this->random->size(6)->get();
+                $client = new Predis\Client();
+                $client->set("cloud-{$cloud}-{$shortcode}", $token->getToken());
+                $client->expire("cloud-{$cloud}-{$shortcode}", 600);
+                return $container->get('renderer')->render($response, 'token.phtml', ['shortcode' => $shortcode]);
+            }
+        }
+    );
+
+    $app->get(
+        '/{cloud}/{shortcode}', function (Request $request, Response $response, $args) {
+
+            $cloud = $args['cloud'];
+
+            $client = new Predis\Client();
+            $shortcode = $args['shortcode'];
+            $token = $client->get("cloud-{$cloud}-{$shortcode}");
+            return $response->withJson(['error' => false, 'access_token' => $token]);
         }
     );
 };
--- src/settings.php
+++ src/settings.php
@@ -21,39 +21,27 @@ return [
           'client_id' => 'vgij09edeilbrtm',
           'client_secret' => getenv('DROPBOX_SECRET'),
           'redirect_uri' => 'https://cloud.scummvm.org/dropbox/token',
-          'auth_uri' => 'https://www.dropbox.com/1/oauth2/authorize',
-          'token_uri' => 'https://api.dropboxapi.com/oauth2/token',
-          'refresh_uri' => ''
         ],
 
         // Box settings
         'box' => [
           'client_id' => 'ep9cz17to1wakzqbq2a5jn5u01b0omxw',
           'client_secret' => getenv('BOX_SECRET'),
-          'redirect_uri' => 'https://cloud.scummvm.org/box/token',
-          'auth_uri' => 'https://www.dropbox.com/1/oauth2/authorize',
-          'token_uri' => 'https://api.dropboxapi.com/oauth2/token',
-          'refresh_uri' => ''
+          'redirect_uri' => 'https://cloud.scummvm.org/box',
         ],
 
         // Google Drive settings
-        'google_drive' => [
-          'client_id' => '102752616291108817673',
+        'gdrive' => [
+          'client_id' => 'AIzaSyBbhRYGs1Xi_RxNMRu_Jhh3tQne2Ipgzfc',
           'client_secret' => getenv('GOOGLE_DRIVE_SECRET'),
-          'redirect_uri' => 'https://cloud.scummvm.org/google_drive/token',
-          'auth_uri' => 'https://accounts.google.com/o/oauth2/auth',
-          'token_uri' => 'https://oauth2.googleapis.com/token',
-          'refresh_uri' => ''
+          'redirect_uri' => 'https://cloud.scummvm.org/gdrive',
         ],
 
         // Onedrive settings
         'onedrive' => [
           'client_id' => '12c88b6d-3037-4c0c-9076-cc4205cfb1d0',
           'client_secret' => getenv('ONEDRIVE_SECRET'),
-          'redirect_uri' => 'https://cloud.scummvm.org/onedrive/token',
-          'auth_uri' => 'https://www.dropbox.com/1/oauth2/authorize',
-          'token_uri' => 'https://api.dropboxapi.com/oauth2/token',
-          'refresh_uri' => ''
+          'redirect_uri' => 'https://cloud.scummvm.org/onedrive',
         ],
     ],
 ];
