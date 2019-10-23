369065dab3523a4104c7431390e42677876f1342
Eugene Sandulenko <sev@scummvm.org>
2019-10-23 19:13:24 +0200

    MULTIPLAYER: Remove more cloud code, added /lobbies

--- src/routes.php
+++ src/routes.php
@@ -4,58 +4,6 @@ use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-function getCloudProviderAndScope($cloudProviderName, $container)
-{
-    $oauth = $container->get('settings')[$cloudProviderName];
-    $scope = [];
-    switch ($cloudProviderName) {
-    case 'dropbox':
-        $provider = new Stevenmaguire\OAuth2\Client\Provider\Dropbox(
-            [
-            'clientId'          => $oauth['client_id'],
-            'clientSecret'      => $oauth['client_secret'],
-            'redirectUri'       => $oauth['redirect_uri']
-            ]
-        );
-        break;
-    case 'box':
-        $provider = new Stevenmaguire\OAuth2\Client\Provider\Box(
-            [
-            'clientId'          => $oauth['client_id'],
-            'clientSecret'      => $oauth['client_secret'],
-            'redirectUri'       => $oauth['redirect_uri']
-            ]
-        );
-        break;
-    case 'gdrive':
-        $provider = new League\OAuth2\Client\Provider\Google(
-            [
-            'clientId'          => $oauth['client_id'],
-            'clientSecret'      => $oauth['client_secret'],
-            'redirectUri'       => $oauth['redirect_uri'],
-            'accessType'        => 'offline',
-            ]
-        );
-        $scope = [ 'prompt' => 'consent', 'scope' => [ 'https://www.googleapis.com/auth/drive.file' ] ];
-        break;
-    case 'onedrive':
-        $provider = new Stevenmaguire\OAuth2\Client\Provider\Microsoft(
-            [
-            'clientId'          => $oauth['client_id'],
-            'clientSecret'      => $oauth['client_secret'],
-            'redirectUri'       => $oauth['redirect_uri']
-            ]
-        );
-        $scope = [ 'scope' => [ 'Files.ReadWrite.AppFolder', 'offline_access' ] ];
-        break;
-    default:
-        return;
-      break;
-    }
-
-    return [ 'provider' => $provider, 'scope' => $scope ];
-}
-
 return function (App $app) {
     $container = $app->getContainer();
 
@@ -69,95 +17,14 @@ return function (App $app) {
         }
     );
 
-    $app->get(
-        '/{cloud}', function (Request $request, Response $response, array $args) use ($container) {
-            $cloud = $args['cloud'];
-            $providerAndScope = getCloudProviderAndScope($cloud, $container);
+	$app->get(
+        '/lobbies', function (Request $request, Response $response, array $args) use ($container) {
+            // Sample log message
+            $container->get('logger')->info("Slim-Skeleton '/lobbies' route");
 
-            if (!isset($providerAndScope)) {
-                return $response->withJson(['error' => true, 'message' => 'Unknown cloud provider']);
-            }
-
-            if (!isset($_GET['code'])) {
-                // If we don't have an authorization code then get one
-                $authUrl = $providerAndScope['provider']->getAuthorizationUrl($providerAndScope['scope']);
-                $_SESSION['oauth2state'] = $providerAndScope['provider']->getState();
-
-                return $response->withRedirect($authUrl);
-
-                // Check given state against previously stored one to mitigate CSRF attack
-            } elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
-
-                unset($_SESSION['oauth2state']);
-                return $response->withJson(['error' => true, 'message' => 'Invalid state']);
-
-            } else {
-                try {
-                    // Try to get an access token (using the authorization code grant)
-                    $token = $providerAndScope['provider']->getAccessToken(
-                        'authorization_code', [
-                        'code' => $_GET['code']
-                        ]
-                    );
-
-                    $this->random = new PragmaRX\Random\Random();
-                    $shortcode = $this->random->size(6)->get();
-                    $client = new Predis\Client();
-                    $client->set("cloud-{$cloud}-{$shortcode}", json_encode($token));
-                    $client->expire("cloud-{$cloud}-{$shortcode}", 600);
-                    return $container->get('renderer')->render($response, 'token.phtml', ['shortcode' => $shortcode]);
-                }
-                catch(League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
-                    return $response->withJson(['error' => true, 'message' => $e->getMessage()]);
-                }
-
-            }
+            // Render index view
+            return $response->withJson(['test' => 123, 'test2' => 124]);
         }
     );
-
-    $app->get(
-        '/{cloud}/token/{shortcode}', function (Request $request, Response $response, $args) {
-            $cloud = $args['cloud'];
-            $client = new Predis\Client();
-            $shortcode = $args['shortcode'];
-            $token = json_decode($client->get("cloud-{$cloud}-{$shortcode}"), true);
-
-            if (!isset($token)) {
-                return $response->withJson(['error' => true, 'message' => 'Token not found']);
-            }
-            $client->del("cloud-{$cloud}-{$shortcode}");
-            return $response->withJson(['error' => false, 'oauth' => $token]);
-        }
-    );
-
-    $app->get(
-        '/{cloud}/refresh', function (Request $request, Response $response, $args) use ($container) {
-
-            $providerAndScope = getCloudProviderAndScope($args['cloud'], $container);
-
-            if (!$request->hasHeader('X-ScummVM-Refresh-Token')) {
-                return $response->withJson(['error' => true, 'message' => 'Missing refresh token']);
-            }
-
-            if (!isset($providerAndScope)) {
-                return $response->withJson(['error' => true, 'message' => 'Unknown cloud provider']);
-            }
-
-            try {
-                $token = $providerAndScope['provider']->getAccessToken(
-                    'refresh_token', [
-                    'refresh_token' => $request->getHeaderLine('X-ScummVM-Refresh-Token')
-                    ]
-                );
-
-                // $json = json_decode(json_encode($token), true);
-                return $response->withJson(['error' => false, 'oauth' => $token]);
-            }
-            catch(League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
-                return $response->withJson(['error' => true, 'message' => $e->getMessage()]);
-            }
-        }
-    );
-
 
 };
--- src/settings.php
+++ src/settings.php
@@ -15,33 +15,5 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
-
-        // Dropbox settings
-        'dropbox' => [
-          'client_id' => 'vgij09edeilbrtm',
-          'client_secret' => getenv('DROPBOX_SECRET'),
-          'redirect_uri' => 'https://cloud.scummvm.org/dropbox',
-        ],
-
-        // Box settings
-        'box' => [
-          'client_id' => 'ep9cz17to1wakzqbq2a5jn5u01b0omxw',
-          'client_secret' => getenv('BOX_SECRET'),
-          'redirect_uri' => 'https://cloud.scummvm.org/box',
-        ],
-
-        // Google Drive settings
-        'gdrive' => [
-          'client_id' => '201747806507-m6mclc7ijtp0v1fbj2qqehrdoh3uhofp.apps.googleusercontent.com',
-          'client_secret' => getenv('GOOGLE_DRIVE_SECRET'),
-          'redirect_uri' => 'https://cloud.scummvm.org/gdrive',
-        ],
-
-        // Onedrive settings
-        'onedrive' => [
-          'client_id' => '12c88b6d-3037-4c0c-9076-cc4205cfb1d0',
-          'client_secret' => getenv('ONEDRIVE_SECRET'),
-          'redirect_uri' => 'https://cloud.scummvm.org/onedrive',
-        ],
     ],
 ];
