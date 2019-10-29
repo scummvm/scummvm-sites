06c8d29c19d0c191c762d3e11de061304308b007
Eugene Sandulenko <sev@scummvm.org>
2019-10-29 23:26:37 +0000

    MULTIPLAYER: Increased rate limit

--- src/middleware.php
+++ src/middleware.php
@@ -4,13 +4,14 @@ use Slim\App;
 
 return function (App $app) {
     // e.g: $app->add(new \Slim\Csrf\Guard);
+
     $app->add(
         \RateLimit\Middleware\RateLimitMiddleware::createDefault(
             \RateLimit\RateLimiterFactory::createRedisBackedRateLimiter(
                 [
                 'host' => 'localhost',
                 'port' => 6379,
-                ], 100, 3600
+			], 500, 60
             ),
             [
               'limitExceededHandler' => function ($request, $response) {
