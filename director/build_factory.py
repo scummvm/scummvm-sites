b21e79ab4044e80df8c46e9cb58ffc1df30428e1
Roland van Laar <roland@rolandvanlaar.nl>
2020-05-24 14:23:30 +0200

    Buildstep speedsups:
    - only run configure on first run and
    - run make with nprocs + 1.

--- director/build_factory.py
+++ director/build_factory.py
@@ -8,6 +8,19 @@ from buildbot.plugins import steps, util
 
 default_step_kwargs: Dict[str, Any] = {"logEnviron": False}
 
+
+@util.renderer
+def makeCommand(props):
+    cpus = int(props.getProperty("CPUs")) + 1
+    if not cpus:
+        cpus = 2
+    return ["make", "-j", str(cpus)]
+
+
+def configure_has_not_been_run(step):
+    return not step.getProperty("configure_has_ran")
+
+
 build_factory = util.BuildFactory()
 # check out the source
 checkout_step = steps.GitHub(
@@ -16,16 +29,39 @@ checkout_step = steps.GitHub(
     **default_step_kwargs,
 )
 build_factory.addStep(checkout_step)
-# run the tests (note that this will require that 'trial' is installed)
+
+build_factory.addStep(
+    steps.SetPropertyFromCommand(
+        name="nproc",
+        description="Finding the number of CPUs",
+        command="nproc",
+        property="nproc",
+        **default_step_kwargs
+    )
+)
+
+build_factory.addStep(
+    # Check for config.mk, which is created when configure runs
+    steps.SetPropertyFromCommand(
+        name="configure?",
+        description="Find if configure has run before",
+        command="[ -f config.mk ] && ls -1 config.mk || exit 0",
+        property="configure_has_ran",
+        **default_step_kwargs
+    )
+)
+
 build_factory.addStep(
     steps.Configure(
-        command=["./configure", "--disable-all-engines", "--enable-engine=director",],
+        command=["./configure", "--disable-all-engines", "--enable-engine=director"],
         env={"CXX": "ccache g++"},
+        doStepIf=configure_has_not_been_run,
         **default_step_kwargs,
     )
 )
-build_factory.addStep(steps.Compile(command=["make"], **default_step_kwargs))
 
+
+build_factory.addStep(steps.Compile(command=makeCommand, **default_step_kwargs))
 
 master_dir = os.path.dirname(os.path.dirname(__file__))
 master_file = os.path.join(master_dir, "scummvm-binary")
