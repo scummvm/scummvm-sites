a9a833e026ab425abebf155b511f7c37f2321bdc
Roland van Laar <roland@rolandvanlaar.nl>
2023-01-03 23:40:13 +0100

    Give each buildmaster a unique name
    Normally they are based on the hostname. Multiple masters on one host
    would result in a naming clash.

---
+++ director/vendor/buildmaster.py
@@ -0,0 +1,18 @@
+"""
+Vendorized BuildMaster.
+
+Used to support giving the buildmaster a unique name.
+"""
+
+from buildbot.master import BuildMaster as OriginalBuildMaster
+
+class BuildMaster(OriginalBuildMaster):
+    """
+    BuildMaster that accepts a unique name
+
+    The BuildMaster name is generated from the hostname and the path on the server.
+    This clashes because we run multiple masters from one directory.
+    """
+    def __init__(self, basedir, configFileName=None, umask=None, reactor=None, config_loader=None, master_name=None):
+        super().__init__(basedir, configFileName, umask, reactor, config_loader)
+        self.name = master_name
