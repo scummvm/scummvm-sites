2b4a4cd8372302567499965347647a18844107ae
Roland van Laar <roland@rolandvanlaar.nl>
2023-01-03 23:34:23 +0100

    Improve handling on error patterns in ScummVMTests
    The errorPattern is now a regular expression and errors are
    shown as a different dropdown on the build pages.
    
    Leverages the WarningCountingShellCommand.

--- director/steps.py
+++ director/steps.py
@@ -1,4 +1,5 @@
 from typing import List, Optional, Any, Dict
+import re
 
 from buildbot import config
 from buildbot.plugins import steps, util
@@ -12,22 +13,84 @@ download_step = steps.FileDownload(
     mastersrc=master_file, workerdest=worker_file, mode=0o0755,
 )
 
-
-class ScummVMTest(steps.Test):
-    """Test steps that treats a specific warning as errors"""
-
-    treat_as_error: List[str] = [
-        "######################  LINGO: syntax error",
-        "BUILDBOT:"
+class ScummVMTest(steps.WarningCountingShellCommand):
+    renderables = [
+        'suppressionFile',
+        'suppressionList',
+        'errorPattern',
+        'warningPattern',
+        'directoryEnterPattern',
+        'directoryLeavePattern',
+        'maxWarnCount',
     ]
 
-    def has_error(self, line):
-        return any(tae in line for tae in self.treat_as_error)
+    errorCount = 0
+    errorPattern = "^WARNING: ######################  LINGO: syntax error.*|^WARNING: BUILDBOT:.*"
+
+    def __init__(self, errorPattern=None, **kwargs):
+        if errorPattern:
+            self.errorPattern = errorPattern
+        super().__init__(**kwargs)
+        self.addLogObserver(
+            'stdio',
+            logobserver.LineConsumerLogObserver(self.errorLogConsumer)
+        )
+
+    def errorLogConsumer(self):
+        ere = self.errorPattern
+        if isinstance(ere, str):
+            ere = re.compile(ere)
+
+        directoryEnterRe = self.directoryEnterPattern
+        if (directoryEnterRe is not None and
+                isinstance(directoryEnterRe, str)):
+            directoryEnterRe = re.compile(directoryEnterRe)
+
+        directoryLeaveRe = self.directoryLeavePattern
+        if (directoryLeaveRe is not None and
+                isinstance(directoryLeaveRe, str)):
+            directoryLeaveRe = re.compile(directoryLeaveRe)
+
+        # Check if each line in the output from this command matched our
+        # warnings regular expressions. If did, bump the warnings count and
+        # add the line to the collection of lines with warnings
+        self.loggedErrors = []
+        while True:
+            _, line = yield
+            if directoryEnterRe:
+                match = directoryEnterRe.search(line)
+                if match:
+                    self.directoryStack.append(match.group(1))
+                    continue
+            if (directoryLeaveRe and
+                self.directoryStack and
+                    directoryLeaveRe.search(line)):
+                self.directoryStack.pop()
+                continue
+
+            match = ere.match(line)
+            if match:
+                self.loggedErrors.append(line)
+                self.errorCount += 1
+
+    @defer.inlineCallbacks
+    def createSummary(self, log):
+        super().createSummary(log)
+        if self.errorCount:
+            yield self.addCompleteLog(f"errors ({self.errorCount})",
+                                       "\n".join(self.loggedErrors) + "\n")
+
+        errors_stat = self.getStatistic('errors', 0)
+        self.setStatistic('errors', errors_stat + self.errorCount)
+
+        old_count = self.getProperty("errors-count", 0)
+        self.setProperty(
+            "errors-count", old_count + self.warnCount, "ScummVMShellCommand")
 
     def evaluateCommand(self, cmd):
         result = super().evaluateCommand(cmd)
-        if any(self.has_error(line) for line in self.loggedWarnings):
-            return worst_status(FAILURE, result)
+        if self.errorCount:
+            result = worst_status(result, FAILURE)
         return result
 
 
