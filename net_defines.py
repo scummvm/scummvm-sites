512fbde9e69995a7ae8173be601ec3f5af586a02
Little Cat <toontownlittlecat@gmail.com>
2023-01-25 07:27:27 -0400

    MULTIPLAYER: Relay support.

---
+++ net_defines.py
@@ -0,0 +1,8 @@
+# Copied from engines/scumm/he/moonbase/net_defines.h
+
+# These are used for relaying.
+PN_PRIORITY_HIGH = 			0x00000001
+PN_SENDTYPE_INDIVIDUAL =	1
+PN_SENDTYPE_GROUP =			2
+PN_SENDTYPE_HOST =			3
+PN_SENDTYPE_ALL =			4
