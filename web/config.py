85b476f3468178c783d7ddc6fa1dada137884e9a
Little Cat <toontownlittlecat@gmail.com>
2023-03-09 04:45:16 -0400

    MULTIPLAYER: Format Python code.

--- web/config.py
+++ web/config.py
@@ -1,23 +1,21 @@
 # Games to accept:
 GAMES = [
-	"football", # Backyard Football (1999)
-	"baseball2001", # Backyard Baseball 2001
-	"football2002", # Backyard Football 2002 
-	"moonbase", # Moonbase Commander (v1.0/v1.1/Demo)
+    "football",  # Backyard Football (1999)
+    "baseball2001",  # Backyard Baseball 2001
+    "football2002",  # Backyard Football 2002
+    "moonbase",  # Moonbase Commander (v1.0/v1.1/Demo)
 ]
 
 # Full names of accepted games.  Make sure that they
 # match with the GAMES list above
 NAMES = {
-	"football": "Backyard Football",
-	"baseball2001": "Backyard Baseball 2001",
-	"football2002": "Backyard Football 2002",
-	"moonbase": "Moonbase Commander"
+    "football": "Backyard Football",
+    "baseball2001": "Backyard Baseball 2001",
+    "football2002": "Backyard Football 2002",
+    "moonbase": "Moonbase Commander",
 }
 
 # Version variants to accept for a specific game.
 # If none exist but game exist in the GAMES list,
 # that means there's only one game version.
-VERSIONS = {
-	"moonbase": ["1.0", "1.1", "Demo"]
-}
+VERSIONS = {"moonbase": ["1.0", "1.1", "Demo"]}
