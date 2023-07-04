203138d1befba5b39b693fb40ba76f3dfdf18927
Abhinav Chennubhotla <51199011+PhoenixFlame101@users.noreply.github.com>
2023-07-04 19:10:32 +0530

    INTEGRITY: Add newlines to HTML tags
    This is so the source sent to the browser is a bit
    more readable

--- pagination.php
+++ pagination.php
@@ -1,6 +1,6 @@
 <?php
 $stylesheet = "style.css";
-echo "<link rel='stylesheet' href='{$stylesheet}'>";
+echo "<link rel='stylesheet' href='{$stylesheet}'>\n";
 
 function create_page($filename, $results_per_page, $count_query, $select_query) {
   $mysql_cred = json_decode(file_get_contents('mysql_config.json'), true);
@@ -36,50 +36,50 @@ function create_page($filename, $results_per_page, $count_query, $select_query) 
   $query = "{$select_query} LIMIT {$results_per_page} OFFSET {$offset}";
   $result = $conn->query($query);
 
-  echo "<table>";
-  echo "<th/>"; // Numbering column
+  echo "<table>\n";
+  echo "<th/>\n"; // Numbering column
 
   $counter = $offset + 1;
   while ($row = $result->fetch_assoc()) {
     if ($counter == $offset + 1) { // If it is the first run of the loop
       foreach ($row as $key => $value) {
-        echo "<th>{$key}</th>";
+        echo "<th>{$key}</th>\n";
       }
     }
 
-    echo "<tr>";
-    echo "<td>{$counter}.</td>";
+    echo "<tr>\n";
+    echo "<td>{$counter}.</td>\n";
     foreach ($row as $key => $value) {
-      echo "<td>{$value}</td>";
+      echo "<td>{$value}</td>\n";
     }
-    echo "</tr>";
+    echo "</tr>\n";
 
     $counter++;
   }
 
-  echo "</table>";
+  echo "</table>\n";
 
   // Navigation elements
-  echo "<div class=pagination>";
+  echo "<div class=pagination>\n";
   if ($page > 1)
-    echo "<a href={$filename}>❮❮</a>";
+    echo "<a href={$filename}>❮❮</a>\n";
   if ($page - 2 > 1)
-    echo "<div class=more>...</div>";
+    echo "<div class=more>...</div>\n";
 
   for ($i = $page - 2; $i <= $page + 2; $i++) {
     if ($i >= 1 && $i <= $num_of_pages)
       if ($i == $page)
-        echo sprintf("<a class=active href=%s?page=%d>%d</a>", $filename, $i, $i);
+        echo sprintf("<a class=active href=%s?page=%d>%d</a>\n", $filename, $i, $i);
       else
-        echo sprintf("<a href=%s?page=%d>%d</a>", $filename, $i, $i);
+        echo sprintf("<a href=%s?page=%d>%d</a>\n", $filename, $i, $i);
   }
 
   if ($page + 2 < $num_of_pages)
-    echo "<div class=more>...</div>";
+    echo "<div class=more>...</div>\n";
   if ($page < $num_of_pages)
-    echo "<a href={$filename}?page={$num_of_pages}>❯❯</a>";
+    echo "<a href={$filename}?page={$num_of_pages}>❯❯</a>\n";
 
-  echo "</div>";
+  echo "</div>\n";
 
 }
 ?>
