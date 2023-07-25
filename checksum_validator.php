2773978c0a979d5317dfe1d2c39dcf0859468c59
Abhinav Chennubhotla <51199011+PhoenixFlame101@users.noreply.github.com>
2023-07-25 18:21:22 +0530

    INTEGRITY: Checksum validation for user filesets
     - Checksums are checked one by one for every file
     - Create json_response object that will be at the endpoint
     - Unique error codes for different mismatches

--- checksum_validator.php
+++ checksum_validator.php
@@ -1,6 +1,4 @@
 <?php
-require('dat_parser.php');
-
 $mysql_cred = json_decode(file_get_contents('mysql_config.json'), true);
 $servername = $mysql_cred["servername"];
 $username = $mysql_cred["username"];
@@ -45,54 +43,77 @@ $json_response = array(
 
 // Check if all files in fullmatch filesets are present with user
 while ($game = $games->fetch_array()) {
-  $fileset = $conn->query("SELECT name, size,
-  checksize, checktype, filechecksum.checksum
-  FROM filechecksum
-  JOIN file ON file.id = filechecksum.file
+  $fileset = $conn->query("SELECT file.id, name, size FROM file
   JOIN fileset ON fileset.id = file.fileset
   WHERE fileset.game = {$game['id']} AND fileset.status = 'fullmatch'");
 
   if ($fileset->num_rows == 0)
     continue;
 
-  $file_object = $json_object->files;
-  print_r($fileset);
+  // Convert checktype, checksize to checkcode
+  $fileset = $fileset->fetch_all(MYSQLI_ASSOC);
+  foreach (array_values($fileset) as $index => $file) {
+    $spec_checksum_res = $conn->query("SELECT checksum, checksize, checktype
+    FROM filechecksum WHERE file = {$file['id']}");
 
-  $mismatch = false;
-  foreach ($file_object as $user_file) {
-    $status = 'ok';
-    $db_file = $fileset->fetch_array();
-
-    if (!($db_file['name'] == $user_file['name'])) {
-      $mismatch = true;
-      $status = 'unknown';
-    }
-
-
-    if ($mismatch)
-      break;
-
-    foreach ($value as $checksum_data) {
-      if ($mismatch)
-        break;
-
-      foreach ($checksum_data as $key => $value) {
-        $user_checkcode = $checksum_data->type;
-        $user_checksum = $checksum_data->checksum;
-
-        list($checksize, $checktype, $checksum) = get_checksum_props($user_checkcode, $user_checksum);
-        $mismatch == !($db_file['checksum'] == $checksum) ||
-          !($db_file['checktype'] == $checktype) ||
-          !($db_file['checksize'] == $checksize);
-
-        if ($mismatch)
-          break;
-      }
+    while ($spec_checksum = $spec_checksum_res->fetch_assoc()) {
+      $fileset[$index][$spec_checksum['checktype'] . '-' . $spec_checksum['checksize']] = $spec_checksum['checksum'];
     }
   }
 
+  $file_object = $json_object->files;
 
+  // Sort the filesets by filename
+  usort($file_object, function ($a, $b) {
+    return strcmp($a->name, $b->name) == -1 ? -1 : 1;
+  });
+  usort($fileset, function ($a, $b) {
+    return strcmp($a['name'], $b['name']) == -1 ? -1 : 1;
+  });
+
+  for ($i = 0, $j = 0; $i < count($fileset), $j < count($file_object); $i++, $j++) {
+    $status = 'ok';
+    $db_file = $fileset[$i];
+    $user_file = $file_object[$j];
+    $filename = $user_file->name;
+
+    if ($db_file['name'] != $user_file->name) {
+      if ($db_file['name'] > $user_file->name) {
+        $status = 'unknown_file';
+        $i--; // Retain same db_file for next iteration
+      }
+      else {
+        $status = 'missing';
+        $filename = $db_file['name'];
+        $j--; // Retain same user_file for next iteration
+      }
+    }
+    elseif ($db_file['size'] != $user_file->size && $status == 'ok') {
+      $status = 'size_mismatch';
+    }
+
+    if ($status == 'ok') {
+      foreach ($user_file->checksums as $checksum_data) {
+        foreach ($checksum_data as $key => $value) {
+          $user_checksum = $checksum_data->checksum;
+          $user_checkcode = $checksum_data->type;
+          if (strpos($user_checkcode, '-') === false)
+            $user_checkcode .= '-0';
+
+          if (strcasecmp($db_file[$user_checkcode], $user_checksum) != 0)
+            $status = 'checksum_mismatch';
+        }
+      }
+    }
+
+    if ($status != 'ok')
+      $json_response['error'] = 1;
+
+    array_push($json_response['files'], array('status' => $status, 'name' => $filename));
+  }
 }
+
+$json_response = json_encode($json_response);
 
 $conn->close();
 ?>
