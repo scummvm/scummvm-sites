d7d1d39dafbb9aeec2ade4a429eb6ed9941c041d
Abhinav Chennubhotla <51199011+PhoenixFlame101@users.noreply.github.com>
2023-07-27 23:09:19 +0530

    INTEGRITY: Change log table order, filters

--- logs.php
+++ logs.php
@@ -5,11 +5,14 @@ $filename = "logs.php";
 $records_table = "log";
 $select_query = "SELECT id, `timestamp`, category, user, `text`
 FROM log";
-$order = "ORDER BY `timestamp` DESC";
+$order = "ORDER BY `timestamp` DESC, id DESC";
 
 $filters = array(
-  "category" => "log",
-  "user" => "log"
+  'id' => 'log',
+  'timestamp' => 'log',
+  'category' => 'log',
+  'user' => 'log',
+  'text' => 'log'
 );
 
 create_page($filename, 25, $records_table, $select_query, $order, $filters);
