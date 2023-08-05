49d8b4dcd47b6b3a34187ce6937195c4e4d3838f
Abhinav Chennubhotla <51199011+PhoenixFlame101@users.noreply.github.com>
2023-08-05 23:28:37 +0530

    INTEGRITY: Fix user fileset insertion

--- endpoints/validate.php
+++ endpoints/validate.php
@@ -114,6 +114,9 @@ while ($game = $games->fetch_array()) {
       $json_response['error'] = 1;
 
     array_push($json_response['files'], array('status' => $status, 'name' => $filename));
+
+    if ($i < count($fileset) && $j < count($file_object))
+      break;
   }
 }
 
