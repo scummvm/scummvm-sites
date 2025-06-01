f950f723fb7a59b3643eafc9e407e457597c229f
Eugene Sandulenko <sev@scummvm.org>
2025-06-01 21:15:06 +0200

    PLANET: Removing old moonmoon-based planet

--- custom/views/archive/footer.tpl.php
+++
@@ -1,3 +0,0 @@
-        <div id="footer">
-            <p><?php echo str_replace('%s', 'href="http://moonmoon.org"', _g('Powered by <a %s>moonmoon</a>'))?> | <a href="./admin/"><?=_g('Administration')?></a></p>
-        </div>
--- custom/views/default/top.tpl.php
+++
@@ -1,3 +0,0 @@
-        <div id="header">
-            <h1 id="top"><a href="<?php echo $PlanetConfig->getUrl(); ?>"><img src="custom/img/scummvm_logo.png" alt="<?php echo $PlanetConfig->getName(); ?>"></a></h1>
-        </div>
