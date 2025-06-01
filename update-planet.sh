be5ff9be21a3671a2c68a52837bd4b2ba50271ba
Lothar Serra Mari <mail@serra.me>
2025-06-01 23:19:29 +0200

    PLANET: Make sure we are in the pluto working directory when running the update

--- update-planet.sh
+++ update-planet.sh
@@ -2,5 +2,6 @@ eval "$(/var/www/.rbenv/bin/rbenv init -)"
 
 rbenv global 3.4.4
 
-pluto build /var/www/pluto/planet.ini -t scummvm -o /var/www/pluto/public_html
+cd /var/www/pluto
 
+pluto build /var/www/pluto/planet.ini -t scummvm -o /var/www/pluto/public_html
